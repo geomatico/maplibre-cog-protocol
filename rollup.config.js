7f7a731ca874c37bdffa5424f78d603f27f129bb
Oscar Fonts <oscar.fonts@geomati.co>
2024-07-08 14:08:44 +0200

    Fix test

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import typescript from '@rollup/plugin-typescript';
+const typescript = require('@rollup/plugin-typescript');
 
-export default {
+module.exports = {
   input: 'src/index.ts',
   output: {
     dir: 'dist',
