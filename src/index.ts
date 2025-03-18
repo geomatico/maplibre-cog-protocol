9570ff0055cce639428907fa7dbbdd91f935fe61
Ben Meyrick <bameyrick@gmail.com>
2025-03-18 10:45:25 +0000

    Export getMetdata function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import cogProtocol from './cogProtocol';
+import { getMetadata } from './read/getMetadata';
 import locationValues from './read/locationValues';
 import { colorScale, colorSchemeNames } from './render/colorScale';
 import setColorFunction from './render/custom/setColorFunction';
 import { clearMask, setMask } from './render/masking';
 
-export { clearMask, cogProtocol, colorScale, colorSchemeNames, locationValues, setColorFunction, setMask };
+export { clearMask, cogProtocol, colorScale, colorSchemeNames, getMetadata, locationValues, setColorFunction, setMask };
